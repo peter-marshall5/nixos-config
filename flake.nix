cdd77fd52cf3596115cce51d0dc7e934167324cd
Peter Marshall <petms@proton.me>
2024-01-13 19:56:39 -0500

    opcc: Update root UUID

--- flake.nix
+++ flake.nix
@@ -24,7 +24,7 @@
     isServer = true;
     hardware = "generic";
     systemConfig = {
-      fs.root.uuid = "5a5189e7-88cb-40cb-99f4-1f98b6706b27";
+      fs.root.uuid = "f7086f18-40c4-462b-9b79-bcc522a2f6ce";
       fs.root.luksUuid = "388cf94a-5063-41ec-8830-1b62b506fe47";
       fs.home.onRoot = true;
       fs.esp.uuid = "64C1-9B6E";
