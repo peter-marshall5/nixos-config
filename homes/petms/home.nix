7414a218f7cca7f375fa2d0ec926a1c936149bd4
Peter Marshall <petms@proton.me>
2024-01-25 13:58:05 -0500

    Add some basic home configs

--- homes/petms/home.nix
+++ homes/petms/home.nix
@@ -17,23 +17,9 @@
 
   # The home.packages option allows you to install Nix packages into your
   # environment.
-  home.packages = [
-    # # Adds the 'hello' command to your environment. It prints a friendly
-    # # "Hello, world!" when run.
-    # pkgs.hello
-
-    # # It is sometimes useful to fine-tune packages, for example, by applying
-    # # overrides. You can do that directly here, just don't forget the
-    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
-    # # fonts?
-    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
-
-    # # You can also create simple shell scripts directly inside your
-    # # configuration. For example, this adds a command 'my-hello' to your
-    # # environment:
-    # (pkgs.writeShellScriptBin "my-hello" ''
-    #   echo "Hello, ${config.home.username}!"
-    # '')
+  home.packages = with pkgs; [
+    nushell
+    helix
   ];
 
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
@@ -51,24 +37,23 @@
     # '';
   };
 
-  # Home Manager can also manage your environment variables through
-  # 'home.sessionVariables'. If you don't want to manage your shell through Home
-  # Manager then you have to manually source 'hm-session-vars.sh' located at
-  # either
-  #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  /etc/profiles/per-user/petms/etc/profile.d/hm-session-vars.sh
-  #
-  home.sessionVariables = {
-    # EDITOR = "emacs";
+  programs.nushell = {
+    enable = true;
+    configFile.source = dotfiles/nushell/config.nu;
+    envFile.source = dotfiles/nushell/env.nu;
   };
+
+  programs.helix = {
+    enable = true;
+    defaultEditor = true;
+    settings = {
+      theme = "catppuccin_frappe";
+    };
+  };
+
+  # home.sessionVariables = {
+  #   EDITOR = "hx";
+  # };
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
