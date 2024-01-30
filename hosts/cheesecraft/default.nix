ba099d780c83f5c0d9a592a9c91804f7155ad93c
Peter Marshall <petms@proton.me>
2024-01-30 14:16:22 -0500

    cheesecraft: Add build battle

--- hosts/cheesecraft/default.nix
+++ hosts/cheesecraft/default.nix
@@ -19,5 +19,10 @@
       title = "§k::§r §eCheese§bcraft§f - §aSurvival§r §k::§r ";
       levelName = "Cheesecraft Season 4";
     };
+    build-battle = {
+      port = 19133;
+      title = " §k::§r §d§lBuild§r  §c§oBattle §k::§r ";
+      levelName = "Build Battle v3";
+    };
   };
 }
