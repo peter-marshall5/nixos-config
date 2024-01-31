fa36640636bb14b9b056ec00b1a4e9021e60f4d6
Peter Marshall <petms@proton.me>
2024-01-31 11:28:02 -0500

    Update home

--- homes/petms/home.nix
+++ homes/petms/home.nix
@@ -20,6 +20,7 @@
   home.packages = with pkgs; [
     nushell
     helix
+    swaybg
   ];
 
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
@@ -72,9 +73,111 @@
     userName = "Peter Marshall";
   };
 
-  # home.sessionVariables = {
-  #   EDITOR = "hx";
-  # };
+  # home.sessionVariables = {};
+
+  wayland.windowManager.hyprland.enable = true;
+  wayland.windowManager.hyprland.settings = {
+    "$mod" = "SUPER";
+    bind =
+      [
+        "$mod, Enter, exec, foot"
+        "$mod, C, exec, flatpak run com.google.Chrome"
+        ", Print, exec, grimblast copy area"
+      ] ++ (
+        # workspaces
+        # binds $mod + [shift +] {1..10} to [move to] workspace {1..10}
+        builtins.concatLists (builtins.genList (
+            x: let
+              ws = let
+                c = (x + 1) / 10;
+              in
+                builtins.toString (x + 1 - (c * 10));
+            in [
+              "$mod, ${ws}, workspace, ${toString (x + 1)}"
+              "$mod SHIFT, ${ws}, movetoworkspace, ${toString (x + 1)}"
+            ]
+          )
+          10)
+      );
+    misc = {
+      disable_hyprland_logo = true;
+      disable_splash_rendering = true;
+      vrr = 1;
+    };
+    decoration = {
+      rounding = 2;
+      blur = {
+        size = 12;
+        passes = 2;
+        xray = true;
+        noise = 0.03;
+      };
+    };
+    exec-once = [
+        "waybar"
+        #"swaybg -i ~/.wallpaper"
+      ];
+  };
+
+  programs.waybar.enable = true;
+  programs.waybar.settings = {
+    mainBar = {
+      layer = "top";
+      position = "top";
+      height = 30;
+      modules-left = [ "hyprland/workspaces" ];
+      modules-center = [ "hyprland/window" ];
+      modules-right = [ "mpd" "temperature" ];
+    };
+  };
+
+  programs.foot.enable = true;
+  programs.foot.server.enable = true;
+  programs.foot.settings = {
+    main = {
+      term = "xterm-256color";
+
+      shell = "nu";
+
+      include = "${pkgs.foot.themes}/share/foot/themes/nord";
+
+      font = "Hack:size=13";
+      dpi-aware = "no";
+    };
+
+    mouse = {
+      hide-when-typing = "yes";
+    };
+  };
+
+  home.pointerCursor = {
+    gtk.enable = true;
+    # x11.enable = true;
+    package = pkgs.libsForQt5.breeze-gtk;
+    name = "Breeze";
+    size = 16;
+  };
+
+  gtk = {
+    enable = true;
+    theme = {
+      package = pkgs.libsForQt5.breeze-gtk;
+      name = "Breeze";
+    };
+
+    iconTheme = {
+      package = pkgs.gnome.adwaita-icon-theme;
+      name = "Adwaita";
+    };
+
+    font = {
+      name = "Sans";
+      size = 11;
+    };
+  };
+
+  services.mpd.enable = true;
+  xdg.userDirs.enable = true;
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
