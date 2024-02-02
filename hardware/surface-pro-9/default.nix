0ed9347772bb13378bf88198d44ea9bcd845da50
Peter Marshall <petms@proton.me>
2024-02-02 13:53:20 -0500

    sp9: Add audio DSP

--- hardware/surface-pro-9/default.nix
+++ hardware/surface-pro-9/default.nix
@@ -2,6 +2,8 @@
 
 {
 
+  imports = [ ./sound ];
+
   boot.initrd.availableKernelModules = [ "xhci_pci" "thunderbolt" "nvme" "usb_storage" "sd_mod" ];
 
   boot.initrd.kernelModules = [ "nvme" "xhci_pci" "hid_generic" "atkbd" "surface_aggregator" "surface_aggregator_registry" "surface_aggregator_hub" "surface_hid_core" "8250_dw" "surface_hid" "intel_lpss" "intel_lpss_pci" "pinctrl_tigerlake" "usbhid" ];
