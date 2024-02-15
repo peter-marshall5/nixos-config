e61f14f871dac9066e6da820e8ea7ece28c22cd8
Peter Marshall <petms@proton.me>
2024-02-15 09:51:36 -0500

    svc: Add qemu guest profile, use ext4 root

--- roles/hypervisor/services/vm-base.nix
+++ roles/hypervisor/services/vm-base.nix
@@ -4,6 +4,7 @@
     (modulesPath + "/image/repart.nix")
     (modulesPath + "/profiles/minimal.nix")
     (modulesPath + "/profiles/headless.nix")
+    (modulesPath + "/profiles/qemu-guest.nix")
   ];
 
   image.repart = {
@@ -13,7 +14,7 @@
         storePaths = [ config.system.build.toplevel ];
         repartConfig = {
           Type = "root";
-          Format = "erofs";
+          Format = "ext4";
           Minimize = "guess";
           MakeDirectories = "/home /etc /var";
         };
