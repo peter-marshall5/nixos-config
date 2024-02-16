a645c8584e3f0d0d69a0aa3b2ad088a8a3e38356
Peter Marshall <petms@proton.me>
2024-02-16 15:07:35 -0500

    Make services more debuggable

--- roles/hypervisor/services/default.nix
+++ roles/hypervisor/services/default.nix
@@ -28,8 +28,7 @@ let
     };
   };
 
-  mkService = name: { type ? name, config }:
-  let
+  mkService = name: { type ? name, config }: rec {
     system = lib.nixosSystem {
       inherit (pkgs.hostPlatform) system;
       modules = [
@@ -44,25 +43,29 @@ let
         }
       ];
     };
-    rootfs = "${system.config.system.build.image}/image.raw";
-    linux = "${system.config.boot.kernelPackages.kernel}/${system.config.system.boot.loader.kernelFile}";
-    initrd = "${system.config.system.build.initialRamdisk}/${system.config.system.boot.loader.initrdFile}";
-    cmdline = "init=${system.config.system.build.toplevel}/init ${toString system.config.boot.kernelParams}";
-    state = "${cfg.stateDir}/${name}.img";
-    memory = system.config.svc.memory;
-    storage = system.config.svc.storage;
-    threads = system.config.svc.threads;
-  in lib.nameValuePair "service-${name}" {
-    wantedBy = [ "multi-user.target" ];
-    restartIfChanged = false;
-    script = (''
+    runner = let
+      state = "${cfg.stateDir}/${name}.img";
+      memory = system.config.svc.memory;
+      storage = system.config.svc.storage;
+      threads = system.config.svc.threads;
+      rootfs = "${system.config.system.build.image}/image.raw";
+      linux = "${system.config.boot.kernelPackages.kernel}/${system.config.system.boot.loader.kernelFile}";
+      initrd = "${system.config.system.build.initialRamdisk}/${system.config.system.boot.loader.initrdFile}";
+      cmdline = "init=${system.config.system.build.toplevel}/init ${toString system.config.boot.kernelParams}";
+    in pkgs.writeScriptBin "run-service-${name}" ''
       if ! [ -e "${state}" ]; then
         touch "${storage}"
         ${pkgs.e2fsprogs}/bin/chattr +C "${storage}"
         ${pkgs.util-linux}/bin/fallocate -l "${storage}" "${state}"
       fi
       ${tinyQemu}/bin/qemu-kvm -enable-kvm -drive file="${rootfs}",if=virtio,format=raw,media=disk,readonly=on -drive file="${state}",if=virtio,format=raw,media=disk -nic tap,id=net0,ifname="vm-$name",model=virtio,script=no,downscript=no -nographic -vga none -serial stdio -monitor none -cpu host -m "${memory}" -smp "${toString threads}" -kernel "${linux}" -initrd "${initrd}" -append "${cmdline}"
-    '');
+    '';
+  };
+
+  mkServiceUnit = name: service: lib.nameValuePair "service-${name}" {
+    wantedBy = [ "multi-user.target" ];
+    restartIfChanged = false;
+    serviceConfig.ExecStart = service.runner;
   };
 
 in {
@@ -88,7 +91,9 @@ in {
       mkdir -p ${cfg.stateDir}
     '';
 
-    systemd.services = lib.mapAttrs' mkService cfg.vms;
+    system.build.services = lib.mapAttrs mkService cfg.vms;
+
+    systemd.services = lib.mapAttrs' mkServiceUnit config.system.build.services;
 
   };
 
